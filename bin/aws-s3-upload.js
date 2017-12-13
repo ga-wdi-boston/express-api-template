7af10485fe226edac3d946ffacc2c0fb4283ee62
Chris <06cpayne@gmail.com>
2017-12-13 12:08:17 -0500

    Adds Upload model and mongoose interaction
    Adds the Upload model and schema, and begins the interaction between
    the bin/ script and the db. Next step is to actually take what is being
    returned from s3 and save the necessary pieces in the db.

--- bin/aws-s3-upload.js
+++ bin/aws-s3-upload.js
@@ -1,6 +1,11 @@
+// run using:
+// NODE_PATH=. node bin/aws-s3-upload.js public/huckleberry.gif wutdis
+
 'use strict'
 
 const s3Upload = require('../lib/s3-upload')
+const Upload = require('../app/models/upload')
+const mongoose = require('../app/middleware/mongoose')
 
 const options = {
   path: process.argv[2],
@@ -8,5 +13,12 @@ const options = {
 }
 
 s3Upload(options)
+  .then((s3Response) => {
+    return Upload.create({
+      url: 'someshit',
+      title: 'somet title'
+    })
+  })
   .then(console.log)
   .catch(console.error)
+  .then(() => mongoose.connection.close())
