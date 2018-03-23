c6c0706ddd9857ece780078de4dd4eb9ad9dc4e3
Chris <06cpayne@gmail.com>
2018-03-23 14:03:51 -0400

    Sets up route and action for upload create
    Right now the create action is only an echo server. It makes no request
    to aws, and doesn't save anything in the db. However, multer is working
    and saving the file locally.
    
    Next steps:
      - make the req to aws
      - save in db

---
+++ app/controllers/uploads.js
@@ -0,0 +1,67 @@
+'use strict'
+
+const controller = require('lib/wiring/controller')
+const models = require('app/models')
+const Upload = models.upload
+
+const multer = require('multer')
+const multerUpload = multer({ dest: '/tmp/' })
+
+const authenticate = require('./concerns/authenticate')
+const setUser = require('./concerns/set-current-user')
+const setModel = require('./concerns/set-mongoose-model')
+
+const index = (req, res, next) => {
+  Upload.find()
+    .then(uploads => res.json({
+      uploads: uploads.map((e) =>
+        e.toJSON({ virtuals: true, user: req.user }))
+    }))
+    .catch(next)
+}
+
+const show = (req, res) => {
+  res.json({
+    upload: req.upload.toJSON({ virtuals: true, user: req.user })
+  })
+}
+
+const create = (req, res, next) => {
+  console.log('req.body is ', req.body)
+  console.log('req.file is ', req.file)
+  // s3Upload?
+  // Upload.create()
+  //   .then(upload =>
+  //     res.status(201)
+  //       .json({
+  //         upload: upload.toJSON({ virtuals: true, user: req.user })
+  //       }))
+  //   .catch(next)
+}
+
+const update = (req, res, next) => {
+  delete req.body._owner  // disallow owner reassignment.
+  req.upload.update(req.body.upload)
+    .then(() => res.sendStatus(204))
+    .catch(next)
+}
+
+const destroy = (req, res, next) => {
+  req.upload.remove()
+    .then(() => res.sendStatus(204))
+    .catch(next)
+}
+
+module.exports = controller({
+  index,
+  show,
+  create,
+  update,
+  destroy
+}, { before: [
+  { method: setUser, only: ['index', 'show'] },
+  { method: authenticate, except: ['index', 'show', 'create'] },
+  { method: multerUpload.single('image[file]'), only: ['create'] },
+  { method: setModel(Upload), only: ['show'] },
+  { method: setModel(Upload, { forUser: true }), only: ['update', 'destroy'] }
+] })
--- config/routes.js
+++ config/routes.js
@@ -18,4 +18,5 @@ module.exports = require('lib/wiring/routes')
 .resources('users', { only: ['index', 'show'] })
 
 // all routes created
-
+// .post('/uploads', 'uploads#create')
+.resources('uploads', { only: ['create'] })
