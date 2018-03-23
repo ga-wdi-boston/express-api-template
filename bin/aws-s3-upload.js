179b583dbe7615f6b4e70cf16a17c17a9771ff9d
Chris <06cpayne@gmail.com>
2018-03-23 16:16:10 -0400

    Refactor bin/ and lib/ scripts
    Previously everything was in the bin/ script. However, we want to be
    able to execute the lib code from anywhere in our project. So instead,
    we've pulled the code associated with the s3Upload into its own module,
    and required it in bin/.

--- bin/aws-s3-upload.js
+++ bin/aws-s3-upload.js
@@ -1,61 +1,21 @@
 'use strict'
-
-require('dotenv').config()
-
-const fs = require('fs')
-const crypto = require('crypto')
-const path = require('path')
-
-const AWS = require('aws-sdk')
-const s3 = new AWS.S3()
-
-const mime = require('mime-types')
-
 const Upload = require('app/models/upload')
 const mongoose = require('app/middleware/mongoose')
 
+const mime = require('mime-types')
+
+const s3Upload = require('lib/aws-upload')
+
 const file = {
   path: process.argv[2],
-  name: process.argv[3]
+  originalname: process.argv[2],
+  name: process.argv[3],
+  mimetype: mime.lookup(process.argv[2])
 }
 
-file.stream = fs.createReadStream(file.path)
-file.ext = path.extname(file.path)
-file.mimeType = mime.lookup(file.path)
+// file.mimetype = mime.lookup(file.path)
 
-const promiseRandomBytes = () => {
-  return new Promise((resolve, reject) => {
-    crypto.randomBytes(16, (err, buf) => {
-      if (err) {
-        reject(err)
-      }
-      resolve(buf.toString('hex'))
-    })
-  })
-}
-
-const promiseS3Upload = (params) => {
-  return new Promise((resolve, reject) => {
-    s3.upload(params, function (err, data) {
-      if (err) {
-        reject(err)
-      }
-      resolve(data)
-    })
-  })
-}
-
-promiseRandomBytes()
-  .then((randomString) => {
-    return {
-      ACL: 'public-read',
-      ContentType: file.mimeType,
-      Bucket: process.env.AWS_BUCKET_NAME,
-      Key: randomString + file.ext,
-      Body: file.stream
-    }
-  })
-  .then(promiseS3Upload)
+s3Upload(file)
   .then((s3Response) => {
     console.log('s3Response is ', s3Response)
     return s3Response
